796d390cb5a4b4cb75844f6b8510585732e68272
timheddergott <tim.heddergott@student.unisg.ch>
2024-12-11 12:30:08 +0100

    Update train_rf.py

--- train_rf.py
+++ train_rf.py
@@ -7,7 +7,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 
 
