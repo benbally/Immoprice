4d1190f64aca0f9333afe9801ac326e0ede2218d
benbally <benjamin.bally@student.unisg.ch>
2024-12-11 11:13:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ page = st.sidebar.radio("Go to", ["USA Map", "Property Price Estimator"])
 
 # Dynamisches Laden der Seiten
 if page == "USA Map":
-    file_path = os.path.join("us_map.py")
+    file_path = os.path.join("usa_map.py")
     with open(file_path, encoding="utf-8") as file:
         exec(file.read(), globals())
 
